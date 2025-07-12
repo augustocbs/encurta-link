52cac1685192f253dbc28527862340abb9e5f6bb
Augusto Cesar <augustoc.bsilveira@gmail.com>
2025-07-12 15:59:00 -0300

    fix: adjusted  migration

--- src/migrations/1752334350123-CreateUsersTable.ts
+++ src/migrations/1752334350123-CreateUsersTable.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateTables1752197185013 implements MigrationInterface {
-  name = 'CreateTables1752197185013';
+export class CreateUsersTable1752334350123 implements MigrationInterface {
+  name = 'CreateUsersTable1752334350123';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`urls\` (\`id\` int NOT NULL AUTO_INCREMENT, \`userId\` int NULL, \`originalUrl\` varchar(767) NOT NULL, \`shortCode\` varchar(6) NOT NULL, \`clicks\` int NOT NULL DEFAULT '0', \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, UNIQUE INDEX \`IDX_aacdba4c65e535a14ccf5db306\` (\`originalUrl\`), UNIQUE INDEX \`IDX_34ced802e4a45bf6a6346f2eb9\` (\`shortCode\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+      `CREATE TABLE \`urls\` (\`id\` int NOT NULL AUTO_INCREMENT, \`userId\` int NULL, \`originalUrl\` text NOT NULL, \`shortCode\` varchar(6) NOT NULL, \`clicks\` int NOT NULL DEFAULT '0', \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` datetime(6) NULL, UNIQUE INDEX \`IDX_34ced802e4a45bf6a6346f2eb9\` (\`shortCode\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
       `CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deleted_at\` datetime(6) NULL, UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
@@ -25,9 +25,6 @@ export class CreateTables1752197185013 implements MigrationInterface {
     await queryRunner.query(`DROP TABLE \`users\``);
     await queryRunner.query(
       `DROP INDEX \`IDX_34ced802e4a45bf6a6346f2eb9\` ON \`urls\``,
-    );
-    await queryRunner.query(
-      `DROP INDEX \`IDX_aacdba4c65e535a14ccf5db306\` ON \`urls\``,
     );
     await queryRunner.query(`DROP TABLE \`urls\``);
   }
